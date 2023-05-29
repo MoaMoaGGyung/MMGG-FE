58b81712d8da8a5a50fad44a1b5c7fb4f5131e46
donoh <201904014@o.cnu.ac.kr>
2023-05-29 22:52:12 +0900

    - chore : MUI, recoil, react-router-dom 패키지 설치 및 프로젝트 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./pages/Main.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <div>404 Not Found</div>,
+        children: [
+            {
+                path: "/",
+                element: <Main />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <RouterProvider router={router} />
+);
